9655889799bea532884c8bdd05836239e00b23a4
Shehroz <raxmatshehroz1@gmail.com>
2025-12-29 22:32:09 +0500

    user

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express from 'express'
+
+import { MongoConnect } from '@/config/db.config'
+import { environments } from '@/config/environment.config'
+import indexRoute from '@/routes'
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/v1', indexRoute)
+
+const mongoConnect = new MongoConnect()
+mongoConnect.init()
+
+const bootstrap = async () => {
+	const mongoConnect = new MongoConnect()
+	await mongoConnect.init()
+
+	const port = environments.PORT
+
+	app.listen(port, () => {
+		console.log(`Server running on port ${port}`)
+	})
+}
+
+bootstrap()
